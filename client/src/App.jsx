00dcaddc37bb790f7f47780949d698cca0d71d1d
Max Kotyk <mxm.kotyk@gmail.com>
2024-05-09 11:19:34 +0200

    Add routing

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,5 +1,56 @@
+import { Outlet, RouterProvider, createBrowserRouter } from "react-router-dom";
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: (
+      <div>
+        Shared Layout
+        <Outlet />
+      </div>
+    ),
+    children: [
+      {
+        index: true,
+        element: <div>Home Page</div>,
+      },
+      {
+        path: "register",
+        element: <div>Register Page</div>,
+      },
+      {
+        path: "login",
+        element: <div>Login Page</div>,
+      },
+      {
+        path: "contacts",
+        element: (
+          <div>
+            Contacts
+            <Outlet />
+          </div>
+        ),
+        children: [
+          {
+            path: ":contactId",
+            element: <div>Contact</div>,
+          },
+          {
+            path: ":contactId/edit",
+            element: <div>Edit Contact</div>,
+          },
+        ],
+      },
+    ],
+  },
+]);
+
 function App() {
-  return <>App</>;
+  return (
+    <>
+      <RouterProvider router={router} />
+    </>
+  );
 }
 
 export default App;
