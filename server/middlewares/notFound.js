ed831681f08e41d5b48f3bad424cfe43ca71aaf0
Max Kotyk <mxm.kotyk@gmail.com>
2024-05-15 17:02:34 +0200

    Add cors and notFound middlewares

---
+++ server/middlewares/notFound.js
@@ -0,0 +1,4 @@
+export const notFound = (req, res) => {
+  res.status(404);
+  res.json({ code: 404, message: "Not Found" });
+};
--- server/server.js
+++ server/server.js
@@ -1,10 +1,23 @@
 import express from "express";
+import cors from "cors";
+import "dotenv/config.js";
+
+import { notFound } from "./middlewares/index.js";
+
 const app = express();
+
+app.use(cors());
+app.use(express.urlencoded({ extended: false }));
+app.use(express.json());
+
+const { PORT } = process.env;
 
 app.get("/", (req, res) => {
   res.send("Hello from server");
 });
 
-app.listen(3000, () => {
-  console.log("Server is listening on port 3000");
+app.use(notFound);
+
+app.listen(PORT, () => {
+  console.log(`Server is listening on port ${PORT}`);
 });
