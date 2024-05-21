d6d6167fa67ec9d84a0410c75e83afaffef5d244
Max Kotyk <mxm.kotyk@gmail.com>
2024-05-21 21:15:16 +0200

    Update Contact mongoose schema

--- server/models/Contact.js
+++ server/models/Contact.js
@@ -1,11 +1,17 @@
 import mongoose from "mongoose";
 const { Schema } = mongoose;
 
-const contactSchema = new Schema({
-  name: String,
-  phoneNumber: String,
-  email: String,
-});
+const contactSchema = new Schema(
+  {
+    name: {
+      type: String,
+      required: [true, "Please provide name for a contact"],
+    },
+    phoneNumber: String,
+    email: String,
+  },
+  { versionKey: false, timestamps: true }
+);
 
 const Contact = mongoose.model("contact", contactSchema);
 
