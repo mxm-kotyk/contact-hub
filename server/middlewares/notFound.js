42589067e8734af5f54493d8efa6759083e5b9c5
Maksym Kotyk <mxm.kotyk@gmail.com>
2024-05-22 14:58:41 +0200

    Update notFound error object message

--- server/middlewares/notFound.js
+++ server/middlewares/notFound.js
@@ -1,6 +1,6 @@
 const notFound = (req, res) => {
   res.status(404);
-  res.json({ code: 404, message: "Not Found" });
+  res.json({ status: "Error", statusCode: 404, message: "Not Found" });
 };
 
 export default notFound;
