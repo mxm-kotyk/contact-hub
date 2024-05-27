1d45c9d65daff9bc9d7a963f0093119037e6b575
Max Kotyk <mxm.kotyk@gmail.com>
2024-05-27 12:02:50 +0200

    Add getOneContact, addContact, updateContact, setFavorite and deleteContact endpoints to the contactsApi

--- client/src/redux/contactsApi.js
+++ client/src/redux/contactsApi.js
@@ -1,18 +1,63 @@
 import { createApi, fetchBaseQuery } from "@reduxjs/toolkit/query/react";
 
+const transformResponses = {
+  transformResponse: (response) => response.data,
+  transformErrorResponse: (response) => response.data,
+};
+
 export const contactsApi = createApi({
   reducerPath: "contactApi",
   baseQuery: fetchBaseQuery({
-    baseUrl: "https://contact-hub-46b8.onrender.com/api/",
+    baseUrl: "http://localhost:3000/api/",
   }),
   endpoints: (builder) => ({
     getAllContacts: builder.query({
-      query: () => "contacts/",
-      transformErrorResponse: (error) => {
-        return error.data;
-      },
+      query: () => "contacts",
+      ...transformResponses,
+    }),
+    getOneContact: builder.query({
+      query: (contactId) => `contacts/${contactId}`,
+      ...transformResponses,
+    }),
+    addContact: builder.mutation({
+      query: (body) => ({
+        url: "contacts",
+        method: "POST",
+        body,
+      }),
+      ...transformResponses,
+    }),
+    updateContact: builder.mutation({
+      query: (contactId, body) => ({
+        url: `contacts/${contactId}`,
+        method: "PUT",
+        body,
+      }),
+      ...transformResponses,
+    }),
+    setFavorite: builder.mutation({
+      query: (contactId, body) => ({
+        url: `contacts/${contactId}/favorite`,
+        method: "PATCH",
+        body,
+      }),
+      ...transformResponses,
+    }),
+    deleteContact: builder.mutation({
+      query: (contactId) => ({
+        url: `contacts/${contactId}`,
+        method: "DELETE",
+      }),
+      ...transformResponses,
     }),
   }),
 });
 
-export const { useGetAllContactsQuery } = contactsApi;
+export const {
+  useGetAllContactsQuery,
+  useGetOneContactQuery,
+  useAddContactMutation,
+  useUpdateContactMutation,
+  useSetFavoriteMutation,
+  useDeleteContactMutation,
+} = contactsApi;
